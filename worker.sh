708a6b506a5ab211b082a3b610d38a22c43ce09f
anup.dubey <anup.hnu@outlook.com>
2018-06-23 00:56:54 +0530

    script

--- worker.sh
+++ worker.sh
@@ -2,10 +2,10 @@
 worker=`curl --max-time 10 --connect-timeout 0 -XGET 'localhost:9600/_node/pipelines?pretty' | grep workers | awk '{print $3}' | sed 's/\,//g'`
 if [ "$worker" = 0 ];
 then
-curl "http://198.57.153.77/sendSMS.php?mobile=9711197051&message=DHL-bat(Worker-count-is-"$worker")%20logstash%20stopped"
+curl "http://196.52.17.73/sendSMS.php?mobile=9711197051&message=app1(Worker-count-is-"$worker")%20logstash%20stopped"
 elif [ "$worker" = "" ]; 
 then
-curl "http://198.57.153.77/sendSMS.php?mobile=9711197051&message=DHL-bat(Connection-timeout)%20logstash%20stopped"
+curl "http://196.52.17.73/sendSMS.php?mobile=9711197051&message=app1(Connection-timeout)%20logstash%20stopped"
 else
 echo "All Ok"
 fi
